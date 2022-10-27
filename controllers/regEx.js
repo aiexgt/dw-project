d9b0d59a79d477a030bfa4e90d57f237f49f4a84
aiexgt <lima14florian14@gmail.com>
2022-10-27 15:47:32 -0600

    Cambios Regex

--- controllers/regEx.js
+++ controllers/regEx.js
@@ -8,11 +8,11 @@ const regEx = {
     usuario: new RegExp("^[A-Z]{5,50}$", "i"),
     password: new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,30}$"),
     correo: new RegExp("^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$"),
-    numeros: new RegExp("^[0-9]+$"),
+    numeros: new RegExp("^[0-9 ]+$"),
     estado: new RegExp("^[01]{1,1}$"),
     codigo: new RegExp("^[A-Z-]{4,18}$"),
     monto: new RegExp("^[0-9.]+$"),
-    direccion: new RegExp("^[A-Z ñáéíóúü]{5,300}$", "i"),
+    direccion: new RegExp("^[A-Z0-9 ñáéíóúü]{5,300}$", "i"),
 
 }
 
